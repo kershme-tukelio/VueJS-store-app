214ae339d3d5faa1476d346446accbcc790aaae7
Jovan Zoric <jzjovanzoric@gmail.com>
2021-01-25 20:08:51 +0100

    Removal of git notifications

--- src/services/CustomerService.js
+++ src/services/CustomerService.js
@@ -14,28 +14,28 @@ class CustomerService {
                 firstName: 'First Name 2',
                 lastName: 'Last Name 2',
                 email: 'email2@example.com',
-                cart: []
+                cart: {}
             },
             {
                 id: 3,
                 firstName: 'First Name 3',
                 lastName: 'Last Name 3',
                 email: 'email3@example.com',
-                cart: []
+                cart: {}
             },
             {
                 id: 4,
                 firstName: 'First Name 4',
                 lastName: 'Last Name 4',
                 email: 'email4@example.com',
-                cart: []
+                cart: {}
             },
             {
                 id: 5,
                 firstName: 'First Name 5',
                 lastName: 'Last Name 5',
                 email: 'email5@example.com',
-                cart: []
+                cart: {}
             },
         ]
     }
